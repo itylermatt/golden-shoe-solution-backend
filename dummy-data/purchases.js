6173178f7eae34263d170e7bf794416d155e4c9e
itylermatt <matthewstylerian@gmail.com>
2021-02-19 00:49:37 +0200

    Added Purchases route

---
+++ dummy-data/purchases.js
@@ -0,0 +1,8 @@
+const purchases = [
+    {id: '3245r4gdsve', by: 'abc', date: '5 days ago'},
+    {id: '3245r4adavq', by: 'def', date: '2 days ago'},
+    {id: '3245r4gcsbv', by: 'ghi', date: '1 min ago'},
+    {id: '3245r4gdsvb', by: 'jkl', date: '2 months ago'},
+];
+
+module.exports = {purchases};
--- routes/purchases.js
+++ routes/purchases.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const purchases = require('../dummy-data/purchases');
 
 /* GET purchases. */
 router.get('/', (req,res) => {
-   res.json({title: 'Purchases'});
+   res.json({purchases});
 });
 
 module.exports = router;
