3bc020a8fefb9f68c79a55896d10375047073e15
itylermatt <matthewstylerian@gmail.com>
2021-02-19 00:28:14 +0200

    modified index to return data for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const titleData = require('../dummy-data/index').titleData;
+const categories = require('../dummy-data/index').categories;
+const categoryItems = require('../dummy-data/index').categoryItems;
 
 /* GET home. */
 router.get('/', function(req, res, next) {
-  res.json( { title: 'Welcome to Golden Shoe' });
+  res.json( { titleData, categoryItems, categories });
 });
 
 module.exports = router;
