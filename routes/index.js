6e908de6ba572f242b660386a704fb39b12a1113
itylermatt <matthewstylerian@gmail.com>
2021-02-18 21:34:32 +0200

    Init

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json( { title: 'Golden Shoe' });
+  res.json( { title: 'Welcome to Golden Shoe' });
 });
 
 module.exports = router;
